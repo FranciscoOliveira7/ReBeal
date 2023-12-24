893a7a339bb6ac755f21493068d049010f942c43
Francisco <franciscoliveira7777@gmail.com>
2023-12-24 18:54:56 +0000

    Created post view

---
+++ app/src/main/java/ipca/project/rebeal/ui/PostActivity.kt
@@ -0,0 +1,12 @@
+package ipca.project.rebeal.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ipca.project.rebeal.R
+
+class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.row_post)
+    }
+}
