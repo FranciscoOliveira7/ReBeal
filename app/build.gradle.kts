2c6750bb9d82200c8b3ba631220968921e9d0dcc
Francisco <franciscoliveira7777@gmail.com>
2024-01-05 14:20:13 +0000

    Added Username on registry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,4 +61,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-storage")
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
 }
--- app/src/main/java/ipca/project/rebeal/RegisterActivity.kt
+++ app/src/main/java/ipca/project/rebeal/RegisterActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
+import com.google.firebase.database.FirebaseDatabase
 import ipca.project.rebeal.databinding.RegisterProfileBinding
 import ipca.project.rebeal.ui.isPasswordValid
 import ipca.project.rebeal.ui.isValidEmail
@@ -16,70 +17,83 @@ import ipca.project.rebeal.ui.isValidEmail
 
 class RegisterActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    lateinit var binding: RegisterProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        auth = Firebase.auth
 
-        private lateinit var auth: FirebaseAuth
-        lateinit var binding: RegisterProfileBinding
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
+        binding = RegisterProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-            auth = Firebase.auth
+        binding.RegistarButton.setOnClickListener {
 
-            binding = RegisterProfileBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+            val username = binding.editTextUserName.text.toString()
+            val email = binding.editTextEmailAddress.text.toString()
+            val password = binding.editTextPasswordRegisto.text.toString()
+            val password2 = binding.editTextPasswordRegistoConfirm.text.toString()
 
-            binding.RegistarButton.setOnClickListener {
-
-                val username = binding.utilizadorID.text.toString()
-                val email = binding.editTextEmailAddress.text.toString()
-                val password = binding.editTextPasswordRegisto.text.toString()
-                val password2 = binding.editTextPasswordRegistoConfirm.text.toString()
-
-                if (password != password2){
-                    Toast.makeText(
-                        baseContext,
-                        "Passwords do not match.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    return@setOnClickListener
-                }
-
-                if (!password.isPasswordValid()){
-                    Toast.makeText(
-                        baseContext,
-                        "Password must have at least 6 chars.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    return@setOnClickListener
-                }
-
-                if (!email.isValidEmail()){
-                    Toast.makeText(
-                        baseContext,
-                        "Email is not valid.",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                    return@setOnClickListener
-                }
-
-                auth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            // Sign in success, update UI with the signed-in user's information
-                            val intent = Intent(this@RegisterActivity, MainActivity::class.java)
-                            intent.putExtra("username", username)
-                            startActivity(intent)
-                            finish()
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                            Toast.makeText(
-                                baseContext,
-                                "Authentication failed.${task.exception}",
-                                Toast.LENGTH_SHORT,
-                            ).show()
-                        }
-                    }
+            if (password != password2){
+                Toast.makeText(
+                    baseContext,
+                    "Passwords do not match.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                return@setOnClickListener
             }
+
+            if (!password.isPasswordValid()){
+                Toast.makeText(
+                    baseContext,
+                    "Password must have at least 6 chars.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                return@setOnClickListener
+            }
+
+            if (!email.isValidEmail()){
+                Toast.makeText(
+                    baseContext,
+                    "Email is not valid.",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+
+                        saveUserToFirebaseDatabase()
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.${task.exception}",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
         }
     }
+
+    private fun saveUserToFirebaseDatabase() {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, binding.editTextUserName.text.toString())
+
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("RegisterActivity", "User saved into the Database")
+            }
+    }
+
+    class User(val uid: String, val username: String, val profileUrl: String = "")
+}
