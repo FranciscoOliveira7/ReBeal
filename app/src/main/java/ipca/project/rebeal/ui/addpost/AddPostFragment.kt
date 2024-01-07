71f506193ae2f32f6af82159e1b3e1eb4e58dc73
Francisco <franciscoliveira7777@gmail.com>
2024-01-07 17:57:22 +0000

    added image upload to add post

--- app/src/main/java/ipca/project/rebeal/ui/addpost/AddPostFragment.kt
+++ app/src/main/java/ipca/project/rebeal/ui/addpost/AddPostFragment.kt
@@ -1,11 +1,14 @@
 package ipca.project.rebeal.ui.addpost
 
+import android.content.Intent
+import android.media.Image
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -26,5 +29,27 @@ class AddPostFragment : Fragment() {
     ): View {
         _binding = FragmentAddPostBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSelectImage.setOnClickListener {
+            uploadImage()
+        }
+    }
+
+    private fun uploadImage() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            binding.imageView2.setImageURI(data?.data)
+        }
     }
 }
